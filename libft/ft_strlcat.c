2dfe57d6acfd97deb70653d4bc78b74d66cce9c7
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2022-10-27 23:06:43 +0200

    Protect the function and some minor edits

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 12:21:17 by ladloff           #+#    #+#             */
-/*   Updated: 2022/10/20 20:59:44 by ladloff          ###   ########.fr       */
+/*   Updated: 2022/10/27 17:17:21 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dst_len;
 	size_t	total_len;
 
+	if (!dst && dstsize == 0)
+		return (0);
 	i = -1;
 	dst_len = ft_strlen(dst);
 	total_len = ft_strlen(src) + dst_len;
-	if (!dstsize)
+	if (dstsize == 0)
 		return (ft_strlen(src));
 	if (dstsize < ft_strlen(dst))
 		return (ft_strlen(src) + dstsize);
-	while (src[++i] && dst_len + i < dstsize -1)
+	while (src[++i] && dst_len + i < dstsize - 1)
 		dst[dst_len + i] = src[i];
 	dst[dst_len + i] = '\0';
 	return (total_len);
