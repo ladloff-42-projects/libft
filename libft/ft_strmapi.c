4ca7bd711a4b22592cdee1b7ce702e15f2137502
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2022-10-27 23:07:41 +0200

    Corrected a bad malloc size and added a \0 to the end of the string

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 20:30:48 by ladloff           #+#    #+#             */
-/*   Updated: 2022/10/27 14:03:40 by ladloff          ###   ########.fr       */
+/*   Updated: 2022/10/27 17:46:28 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,11 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	i = -1;
 	p_s = (char *)s;
 	len = ft_strlen(s);
-	str = malloc(len * sizeof(char));
+	str = malloc((len + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
 	while (s[++i])
 		str[i] = f(i, p_s[i]);
+	str[i] = '\0';
 	return (str);
 }
