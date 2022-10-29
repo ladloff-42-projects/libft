c5d047f7d6367719f1a82bbe381ba51a9a1235b9
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2022-10-29 11:48:04 +0200

    Fix norm error

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 23:32:40 by ladloff           #+#    #+#             */
-/*   Updated: 2022/10/27 21:17:22 by ladloff          ###   ########.fr       */
+/*   Updated: 2022/10/29 11:41:04 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int	ft_atoi(const char *str)
 {
-	int	res;
-	int	sign;
+	int		res;
+	int		sign;
 	size_t	i;
 
-	i = - 1;
+	i = -1;
 	while (str[++i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		;
 	sign = 1;
