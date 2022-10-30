c2061562862e22dbfb26023180ac2196b619afb9
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2022-10-30 09:10:36 +0100

    Resize code

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 23:32:40 by ladloff           #+#    #+#             */
-/*   Updated: 2022/10/29 11:41:04 by ladloff          ###   ########.fr       */
+/*   Updated: 2022/10/30 09:06:43 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,15 @@ int	ft_atoi(const char *str)
 {
 	int		res;
 	int		sign;
-	size_t	i;
 
-	i = -1;
-	while (str[++i] == 32 || (str[i] >= 9 && str[i] <= 13))
-		;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
 	sign = 1;
-	if (str[i] == '-' || str[i] == '+')
-		if (str[i++] == '-')
+	if (*str == '-' || *str == '+')
+		if (*str++ == '-')
 			sign = -sign;
 	res = 0;
-	while (ft_isdigit(str[i]))
-		res = res * 10 + str[i++] - 48;
+	while (ft_isdigit(*str))
+		res = res * 10 + *str++ - 48;
 	return (sign * res);
 }
