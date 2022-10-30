b514c025158d2c546ca27208cc30abd2e3197a70
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2022-10-30 16:17:12 +0100

    Clean code by call others functions

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 21:00:58 by ladloff           #+#    #+#             */
-/*   Updated: 2022/10/27 14:03:14 by ladloff          ###   ########.fr       */
+/*   Updated: 2022/10/30 15:57:34 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	ft_putendl_fd(char *s, int fd)
 {
 	if (!s)
 		return ;
-	while (*s)
-		write(fd, s++, sizeof(char));
-	write(fd, "\n", sizeof(char));
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
 }
